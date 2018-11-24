bf794c1ef613604747de3693860a155dd3ad0510
yfaway <yfaway@yahoo.com>
2018-11-23 23:59:36 -0500

    Deprecate the xtend light rules; migrate it over to python instead.

--- automation/jsr223/lights.py
+++ automation/jsr223/lights.py
@@ -5,6 +5,7 @@ from openhab.triggers import when
 from org.eclipse.smarthome.core.items import Metadata
 from org.eclipse.smarthome.core.items import MetadataKey
 from org.eclipse.smarthome.core.library.items import DimmerItem
+from org.joda.time import DateTime
 
 import constants
 reload(constants)
@@ -16,10 +17,161 @@ scriptExtension.importPreset("RuleSimple")
 MetadataRegistry = osgi.get_service("org.eclipse.smarthome.core.items.MetadataRegistry")
 log = LoggerFactory.getLogger("org.eclipse.smarthome.model.script.Rules")
 
+# The period of time in seconds (from the last timestamp a switch was turned
+# off) to ignore the ON command trigged by the motion sensor. This takes care
+# of the scenario when the user manually turns off a light, but that physical
+# spot is covered by a motion sensor, which immediately turns on the light
+# again.
+DELAY_AFTER_LAST_OFF_TIME_IN_MS = 8000 # 8 secs
+
+
+# The light level threshold; if it is below this value, turn on the light.
+ILLUMINANCE_THRESHOLD_IN_LUX = 8
+
+# This Item tag indicates that the switch shares a motion sensor with one
+# or more other switches. These switches then share the same
+# DELAY_AFTER_LAST_OFF_TIME_IN_MS value. I.e. if a switch is just turned
+# off, for the next couple seconds any motion sensor triggering will do
+# nothing. This is because the user is getting out of the zone, and thus it
+# is wrong to turn on another light.
+TAG_SHARED_MOTION_SENSOR = "shared-motion-sensor"
+
+lastOffTimes = {}
+
+@rule("Update light-on time")
+#@when("System started")
+@when("Item VT_Time_Of_Day changed")
+def setLightOnTime(event):
+  state = items["VT_Time_Of_Day"]
+  if state == StringType("EVENING") or state == StringType("NIGHT") \
+        or state == StringType("BED"):
+    events.sendCommand("VT_Time_LightOn", "ON")
+  else:
+    events.sendCommand("VT_Time_LightOn", "OFF")
+
+
 @rule("Turn off all lights when armed away")
 @when("Item {0} changed to {1:d}".format(SECURITY_ITEM_ARM_MODE, SECURITY_STATE_ARM_AWAY))
 def turnOffAllLights(event):
     events.sendCommand(GROUP_LIGHT_SWITCHS, "OFF")
+
+@rule("Turn on light when motion sensor triggered")
+@when("Member of gWallSwitchMotionSensor changed to ON")
+def turnOnSwitchOrRenewTimer(motionSensorEvent):
+    triggeringItem = itemRegistry.getItem(motionSensorEvent.itemName)
+    localIdx = triggeringItem.name.rfind("_")
+    switchName = triggeringItem.name[:localIdx]
+
+    if switchName not in items:
+        log.info("null item")
+        return
+
+    switchItem = itemRegistry.getItem(switchName)
+    
+    if isSwitchOn(switchItem):
+        # renew timer
+        timerName = switchName + "_Timer"
+        events.sendCommand(timerName, "ON")
+        log.info("switch is on")
+        return
+
+    
+    # the associate switch was on; let's do further processing.
+
+    # Is this a fan switch?
+    isFanSwitch = "FanSwitch" in switchName
+
+    # This check needs to be here rather than in the outter scope because
+    # the user might have turned on the light before the programmed 
+    # light on time. In such case, we continue to maintain the timer.
+    if items["VT_Time_LightOn"] != ON and ! isFanSwitch:
+        # check if there is a valid illuminance value
+        illuminanceName = switchName + "_Illuminance"
+        if illuminanceName not in  items:
+            log.info("no illuminance item")
+            return
+
+        illuminanceItem = itemRegistry.getItem(illuminanceName)
+        if UnDefType.NULL == illuminanceItem.state or UnDefType.UNDEF == illuminanceItem.state:
+            log.info("no illuminance value")
+            return
+        elif illuminanceItem.state > DecimalType(ILLUMINANCE_THRESHOLD_IN_LUX):
+            log.info("illuminance value is too high")
+            return
+        # else pass through to turn on the light
+    
+    # If a wall switch was just turned off, ignore the motion sensor event.
+    if switchName in lastOffTimes:
+        timestamp = lastOffTimes[switchName]
+        if (DateTime.now().getMillis() - timestamp <= DELAY_AFTER_LAST_OFF_TIME_IN_MS):
+            log.info("was just off")
+            return
+
+    # An open area might have multiple lights with a shared motion sensor
+    # (e.g. in the security system motion sensor where the motion sensor
+    # tends to be in a corner and cover the whole lobby). In this case, we
+    # only want to trigger a single light if all lights were off. However,
+    # if any light is already on, we still want to renew the timer to keep
+    # the light on.
+    #
+    # Check to see if the motion sensor is allowed to trigger the light.
+    disableAlwaysItemName = triggeringItem.name + "_DisableTriggeringAlways"
+    if disableAlwaysItemName in items and items[disableAlwaysItemName] == ON:
+        log.info("disable motion sensor")
+        return
+
+    # Check to see if there is a dependent relationship between lights.
+    # I.e. if light B is already on, then don't turn on light A if its
+    # motion sensor is triggered.
+    disableIfItemName = triggeringItem.name + "_DisableTriggeringIf"
+    disableTriggeringIfItem = itemRegistry.getItem(disableIfItemName)
+
+    if disableIfItemName in items \
+        and UnDefType.NULL != items[disableIfItemName] \
+        and UnDefType.UNDEF != items[disableIfItemName]:
+        # see if the other light is on
+        theOtherLight = itemRegistry.getItem(items[disableIfItemName].toString())
+
+        if isSwitchOn(theOtherLight):
+            log.info("disabled by other light")
+            return
+      #elif switchItem.hasTag(TAG_SHARED_MOTION_SENSOR):
+        # If it was just turned off, then don't trigger this light yet.
+        # This might be the case that the user is getting out of this zone
+        #if ( lastOffTimes.containsKey(theOtherLight.name) ) {
+        #  val long timestamp = lastOffTimes.get(theOtherLight.name)
+        #  if (now.getMillis() - timestamp <= DELAY_AFTER_LAST_OFF_TIME_IN_MS) {
+        #    return
+      # else - pass through
+
+    events.sendCommand(switchItem.name, "ON")
+
+@rule("Set a timer to turn off the switch after it was programatically turned on")
+@when("Member of gWallSwitch changed")
+def setTimerWhenSwitchIsTurnedOn(switchEvent):
+    triggeringItem = itemRegistry.getItem(switchEvent.itemName)
+
+    timerItemName = triggeringItem.name + "_Timer"
+
+    # Some light might stay on until manually turned off, so the timer item
+    # could be null.
+    if isSwitchOn(triggeringItem):
+        # When in vacation mode, the lights be turned on/off randomly to
+        # simulate presence (thief prevention). The simulation rule has full
+        # control of the lights; thus we don't want to enable the timer.
+        isFanItem = "FanSwitch" in triggeringItem.name
+        if items['VT_In_Vacation'] != ON or isFanItem:
+            if timerItemName in items:
+                events.sendCommand(timerItemName, "ON")
+
+            stringItemName = triggeringItem.name + "_TurnOffOtherLight"
+            if stringItemName in items:
+                events.sendCommand(stringItemName, "OFF")
+    else:
+      if timerItemName in items:
+            events.sendCommand(timerItemName, "OFF")
+
+      lastOffTimes[triggeringItem.name] = DateTime.now().getMillis()
 
 @rule("Turn off wall switch when timer expires")
 @when("Member of gWallSwitchTimer changed to OFF")
@@ -35,14 +187,13 @@ def turnOffWallSwitch(timerEvent):
     motionSensorItems = filter(lambda item: item.name == motionSensorName, 
             ir.getItem(GROUP_WALL_SWITCH_MOTION_SENSORS).members)
 
-    if len(motionSensorItems) > 0 and motionSensorItems[0].state == ON:
+    if len(motionSensorItems) > 0 and motionSensorItems[0] == ON:
         events.sendCommand(triggeringItem.name, "ON")
     else:
         target = filter(lambda item: item.name == switchName, 
                 ir.getItem(GROUP_WALL_SWITCH).members)[0]
         if isSwitchOn(target):
             events.sendCommand(target.name, "OFF")
-
 
 # Returns true if a switch is on.
 # @param switchItem can be a regular OnOffItem or a DimmerItem. In the later
@@ -55,10 +206,10 @@ def isSwitchOn(switchItem):
             and switchItem.state > DecimalType(0))
 
 
-#@rule("Hello World timer rule")
-#@when("Time cron 0/5 * * * * ?")
-#def hellowWorldDecorator(event):
-#    item = itemRegistry.getItem("SF_Lobby_LightSwitch")
+@rule("Hello World timer rule")
+@when("Time cron 0/5 * * * * ?")
+def hellowWorldDecorator(event):
+    item = itemRegistry.getItem("SF_Lobby_LightSwitch")
 
     #turnOffWallSwitch("FF_Foyer_LightSwitch_Timer")
     #meta = MetadataRegistry.get(MetadataKey("light", "SF_Lobby_LightSwitch")) 
@@ -66,3 +217,5 @@ def isSwitchOn(switchItem):
     #log.info("meta: " + str(config["level"]))
 
 
+# TODO remove when @when supports "System started".
+setLightOnTime(None)
