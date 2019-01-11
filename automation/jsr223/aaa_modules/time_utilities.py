5555c2c90716bb3132826dd710c9c542adefb5e9
yfaway <yfaway@yahoo.com>
2019-01-10 23:03:40 -0500

    Add time_utilities::isInTimeRange(timeRangeList, epoch)

--- automation/jsr223/aaa_modules/time_utilities.py
+++ automation/jsr223/aaa_modules/time_utilities.py
@@ -12,3 +12,83 @@ def isKidsSleepTime(epochSeconds = None):
         return dayOfWeek == 5 or dayOfWeek == 6 # weekend
     else:
         return False
+
+# Determines if the current time is in the timeRange string.
+# @param timeRanges string one or multiple time range in 24-hour format.
+#     Example: '10-12', or '6-9, 7-7, 8:30 - 14:45', or '19 - 8' (wrap around)
+# @param epochSeconds int seconds since epoch, optional
+# @return boolean
+def isInTimeRange(timeRanges, epochSeconds = None):
+    if None == timeRanges or 0 == len(timeRanges):
+        raise ValueError('Must have at least one time range.')
+
+    timeStruct = time.localtime(epochSeconds)
+    hour = timeStruct[3]
+    minute = timeStruct[4]
+
+    for range in timeRanges:
+        startHour, startMinute, endHour, endMinute = range
+        if startHour <= endHour: 
+            if hour < startHour:
+                continue
+        else: # wrap around scenario
+            pass
+
+        if minute < startMinute:
+            continue
+
+        if endMinute == 0:
+            if hour >= endHour:
+                continue
+        else: # minutes are > 0
+            if hour > endHour or minute > endMinute:
+                continue
+
+        return True
+
+    return False
+
+# Return a list of time ranges. Each list item is itself a list of 4 elements:
+# startTime, startMinute, endTime, endMinute.
+# @return list
+def _stringToTimeRangeLists(timeRangesString):
+    if None == timeRangesString or 0 == len(timeRangesString):
+        raise ValueError('Must have at least one time range.')
+
+    timeRanges = []
+    pairs = timeRangesString.split(',')
+    for pair in pairs:
+        times = pair.split('-')
+        if 1 == len(times):
+            hour = int(times[0])
+            if hour < 0 or hour > 23:
+                raise ValueError('Hour must be between 0 and 23 inclusive.')
+            timeRanges.append([int(hour), 0, int(hour), 59])
+        elif 2 == len(times):
+            thisRange = []
+
+            def parseHourAndMinute(str):
+                hourMinute = str.split(':')
+                hour = int(hourMinute[0])
+                if hour < 0 or hour > 23:
+                    raise ValueError('Hour must be between 0 and 23 inclusive.')
+
+                if 1 == len(hourMinute):
+                    return [int(hour), 0] # 0 minute
+                elif 2 == len(hourMinute):
+                    minute = int(hourMinute[1])
+                    if minute < 0 or minute > 59:
+                        raise ValueError('Minute must be between 0 and 59 inclusive.')
+                    return [hour, minute]
+                else:
+                    raise ValueError('Must be in format "HH" or "HH:MM".')
+
+            thisRange += parseHourAndMinute(times[0])
+            thisRange += parseHourAndMinute(times[1])
+
+            timeRanges.append(thisRange)
+        else:
+            raise ValueError('Must have either one or two time values.')
+
+    return timeRanges
+
--- automation/jsr223/aaa_modules/time_utilities_test.py
+++ automation/jsr223/aaa_modules/time_utilities_test.py
@@ -32,5 +32,129 @@ class TimeUtilitiesTest(unittest.TestCase):
         dt = datetime.datetime(2018, 12, 25, 15, 10)
         self.assertFalse(time_utilities.isKidsSleepTime(time.mktime(dt.timetuple())))
 
-#run_test(TimeUtilitiesTest, logger) 
+    def testIsInTimeRange_noRange_throwsError(self):
+        with self.assertRaises(ValueError) as cm:
+            time_utilities._stringToTimeRangeLists([])
 
+        self.assertEqual('Must have at least one time range.', cm.exception.args[0])
+
+    def testIsInTimeRange_none_throwsError(self):
+        with self.assertRaises(ValueError) as cm:
+            time_utilities._stringToTimeRangeLists(None)
+
+        self.assertEqual('Must have at least one time range.', cm.exception.args[0])
+
+    def testIsInTimeRange_sameHour_returnTrue(self):
+        dt = datetime.datetime(2018, 12, 25, 7, 58)
+        self.assertTrue(time_utilities.isInTimeRange([[7, 0, 7, 59]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_negativeSameHour_returnFalse(self):
+        dt = datetime.datetime(2018, 12, 25, 7, 58)
+        self.assertFalse(time_utilities.isInTimeRange([[8, 0, 8, 59]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_negativeLargerThanEndMinute_returnFalse(self):
+        dt = datetime.datetime(2018, 12, 25, 7, 58)
+        self.assertFalse(time_utilities.isInTimeRange([[8, 0, 8, 30]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_noEndMinute_returnTrue(self):
+        dt = datetime.datetime(2018, 12, 25, 7, 58)
+        self.assertTrue(time_utilities.isInTimeRange([[7, 0, 8, 0]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_negativeNoEndMinute_returnFalse(self):
+        dt = datetime.datetime(2018, 12, 25, 8, 58)
+        self.assertFalse(time_utilities.isInTimeRange([[7, 0, 8, 0]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_wrapAround_returnTrue(self):
+        dt = datetime.datetime(2018, 12, 25, 7, 58)
+        self.assertTrue(time_utilities.isInTimeRange([[21, 0, 9, 0]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_negativeWrapAround1_returnFalse(self):
+        dt = datetime.datetime(2018, 12, 25, 10, 58)
+        self.assertFalse(time_utilities.isInTimeRange([[21, 0, 9, 0]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_negativeWrapAround2_returnFalse(self):
+        dt = datetime.datetime(2018, 12, 25, 20, 58)
+        self.assertFalse(time_utilities.isInTimeRange([[21, 0, 9, 0]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_multipleRanges_returnTrue(self):
+        dt = datetime.datetime(2018, 12, 25, 7, 58)
+        self.assertTrue(time_utilities.isInTimeRange([[4, 0, 5, 0], [7, 0, 7, 59]],
+                    time.mktime(dt.timetuple())))
+
+    def testIsInTimeRange_negativeMultipleRanges_returnTrue(self):
+        dt = datetime.datetime(2018, 12, 25, 10, 58)
+        self.assertFalse(time_utilities.isInTimeRange([[4, 0, 5, 0], [7, 0, 7, 59]],
+                    time.mktime(dt.timetuple())))
+
+    def testStringToTimeRangeLists_noRange_throwsError(self):
+        with self.assertRaises(ValueError) as cm:
+            time_utilities._stringToTimeRangeLists('')
+
+        self.assertEqual('Must have at least one time range.', cm.exception.args[0])
+
+    def testStringToTimeRangeLists_none_throwsError(self):
+        with self.assertRaises(ValueError) as cm:
+            time_utilities._stringToTimeRangeLists(None)
+
+        self.assertEqual('Must have at least one time range.', cm.exception.args[0])
+
+    def testStringToTimeRangeLists_wrongFormat_throwsError(self):
+        with self.assertRaises(ValueError) as cm:
+            time_utilities._stringToTimeRangeLists('9, 3:2:3-5')
+
+        self.assertEqual('Must be in format "HH" or "HH:MM".', cm.exception.args[0])
+
+    def testStringToTimeRangeLists_invalidHour_throwsError(self):
+        with self.assertRaises(ValueError) as cm:
+            time_utilities._stringToTimeRangeLists('24')
+
+        self.assertEqual('Hour must be between 0 and 23 inclusive.', cm.exception.args[0])
+
+    def testStringToTimeRangeLists_invalidMinute_throwsError(self):
+        with self.assertRaises(ValueError) as cm:
+            time_utilities._stringToTimeRangeLists('7 - 8:60')
+
+        self.assertEqual('Minute must be between 0 and 59 inclusive.', cm.exception.args[0])
+
+    def testStringToTimeRangeLists_oneRangeWithSameHourNoMinutes_returnsExpected(self):
+        list = time_utilities._stringToTimeRangeLists("7")
+        self.assertEqual(1, len(list))
+        self.assertEqual([7, 0, 7, 59], list[0])
+
+    def testStringToTimeRangeLists_oneRangeWithDiffHourNoMinutes_returnsExpected(self):
+        list = time_utilities._stringToTimeRangeLists("7 - 12")
+        self.assertEqual(1, len(list))
+        self.assertEqual([7, 0, 12, 0], list[0])
+
+    def testStringToTimeRangeLists_oneRangeWithDiffHourPartialMinutes_returnsExpected(self):
+        list = time_utilities._stringToTimeRangeLists("7:10 - 12")
+        self.assertEqual(1, len(list))
+        self.assertEqual([7, 10, 12, 0], list[0])
+
+    def testStringToTimeRangeLists_oneRangeWithDiffHourMinutes_returnsExpected(self):
+        list = time_utilities._stringToTimeRangeLists("7:10 - 12:30")
+        self.assertEqual(1, len(list))
+        self.assertEqual([7, 10, 12, 30], list[0])
+
+    def testStringToTimeRangeLists_wrapAroundHour_returnsExpected(self):
+        list = time_utilities._stringToTimeRangeLists("21 - 7")
+        self.assertEqual(1, len(list))
+        self.assertEqual([21, 0, 7, 0], list[0])
+
+    def testStringToTimeRangeLists_multipleRanges_returnsExpected(self):
+        list = time_utilities._stringToTimeRangeLists("7:10 - 8:30, 9, 13 - 15, 16-17:30")
+        self.assertEqual(4, len(list))
+        self.assertEqual([7, 10, 8, 30], list[0])
+        self.assertEqual([9, 0, 9, 59], list[1])
+        self.assertEqual([13, 0, 15, 0], list[2])
+        self.assertEqual([16, 0, 17, 30], list[3])
+
+run_test(TimeUtilitiesTest, logger) 
