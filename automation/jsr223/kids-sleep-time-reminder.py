2fce5d68547ca3be89cc341875815f838c48d7d0
yfaway <yfaway@yahoo.com>
2019-03-28 21:32:29 -0400

    Kids sleep reminder: no longer hardcode the TTS sleep time; use the current time instead.

--- automation/jsr223/kids-sleep-time-reminder.py
+++ automation/jsr223/kids-sleep-time-reminder.py
@@ -1,3 +1,5 @@
+import time
+
 from core.rules import rule
 from core.triggers import when
 
@@ -16,14 +18,30 @@ log = LoggerFactory.getLogger("org.eclipse.smarthome.model.script.Rules")
 @when("Time cron 0 30 19 1/1 * ? *")
 def sayFirstNotice(event):
     if switch_manager.isAnyFirstFloorLightOn():
-        cast_manager.playMessage('Kids, it is 8:30; please put away everything and prepare to go upstairs.',
+        cast_manager.playMessage(createMessageWithTime(
+                    'Kids, it is {}:{}; please put away everything and prepare to go upstairs.'),
                 cast_manager.getFirstFloorCasts())
 
 @rule("Say the second notice and turn off the lights")
 @when("Time cron 0 45 19 1/1 * ? *")
 def saySecondNotice(event):
     if switch_manager.isAnyFirstFloorLightOn():
-        cast_manager.playMessage('Kids, it is 8:45; please go upstairs now.',
+        cast_manager.playMessage(createMessageWithTime(
+                    'Kids, it is {}:{}; please go upstairs now.'),
                 cast_manager.getFirstFloorCasts())
         switch_manager.changeFirstFloorLightsTo('OFF')
+        time.sleep(2)
         switch_manager.changeFoyerLightTo('ON')
+
+# String format two values with the current hour and minute.
+# @param message string
+# @return string
+def createMessageWithTime(message):
+    timeStruct = time.localtime()
+    hourOfDay = timeStruct[3]
+    minute = timeStruct[4]
+
+    if hourOfDay > 12:
+        hourOfDay -= 12
+
+    return message.format(hourOfDay, minute)
