e56ab1e4b14ee2eaaba25fb16152326ad71eb1ca
yfaway <yfaway@yahoo.com>
2019-04-25 22:17:25 -0400

    Initial work to create Zone objects from scope.items.

---
+++ automation/jsr223/aaa_modules/zone_parser.py
@@ -0,0 +1,84 @@
+import re
+
+from core.jsr223 import scope
+from org.slf4j import Logger, LoggerFactory
+
+from aaa_modules.layout_model.zone import Zone, Level
+from aaa_modules.layout_model.astro_sensor import AstroSensor
+from aaa_modules.layout_model.illuminance_sensor import IlluminanceSensor
+from aaa_modules.layout_model.motion_sensor import MotionSensor
+from aaa_modules.layout_model.switch import Fan, Light
+
+logger = LoggerFactory.getLogger("org.eclipse.smarthome.model.script.Rules")
+
+# Construct the zones from the existing items in OpenHab, using this naming
+# convention: Floor_Location_ItemType
+# For example, item "Switch FF_Foyer_LightSwitch ..." will create a zone named
+# 'Foyer' at the first floor; and the zone contains a Light object.
+class ZoneParser:
+    # @param items scope.items
+    # @param itemRegistry scope.itemRegistry
+    # @return a list of aaa_modules.layout_model.zone.Zone objects
+    @staticmethod
+    def parse(items, itemRegistry):
+        zoneMap = {}
+        keyPattern = '([^_]+)_([^_]+)_(.+)' # level_location_deviceName
+
+        for itemName in items.keys():
+            match = re.search('([^_]+)_([^_]+)_(.+)', itemName)
+            if not match:
+                continue
+
+            levelString = match.group(1)
+            location = match.group(2)
+            deviceName = match.group(3)
+            
+            zoneKey = levelString + '_' + location
+            if zoneKey in zoneMap:
+                zone = zoneMap[zoneKey]
+            else:
+                zone = Zone(location, [], ZoneParser.getZoneLevel(levelString))
+
+            openHabItem = itemRegistry.getItem(itemName)
+            if 'LightSwitch' == deviceName:
+                light = Light(openHabItem,
+                        itemRegistry.getItem(itemName + '_Timer'))
+                zone.addDevice(light)
+            elif 'FanSwitch' == deviceName:
+                fan = Fan(openHabItem,
+                        itemRegistry.getItem(itemName + '_Timer'))
+                zone.addDevice(fan)
+            elif 'LightSwitch_Illuminance' == deviceName:
+                illuminanceSensor = IlluminanceSensor(openHabItem)
+                zone.addDevice(illuminanceSensor)
+            elif 'MotionSensor' in deviceName:
+                motionSensor = MotionSensor(openHabItem)
+                zone.addDevice(motionSensor)
+
+            if len(zone.getDevices()) > 0:
+                zoneMap[zoneKey] = zone
+
+        return zoneMap.values()
+                
+
+    # @return aaa_modules.layout_model.zone.Level 
+    @staticmethod
+    def getZoneLevel(levelString):
+        if 'BM' == levelString:
+            return Level.BASEMENT
+        elif 'FF' == levelString:
+            return Level.FIRST_FLOOR
+        elif 'SF' == levelString:
+            return Level.SECOND_FLOOR
+        elif 'TF' == levelString:
+            return Level.THIRD_FLOOR
+        else:
+            return Level.BASEMENT
+
+zones = ZoneParser.parse(scope.items, scope.itemRegistry)
+logger.info("{} zones".format(len(zones)))
+output = ''
+for z in zones:
+    output += '\n' + str(z)
+
+logger.info(output)
