3a65247916992b82d519e2ea3788b7ef0a9a5817
yfaway <yfaway@yahoo.com>
2019-12-06 22:25:20 -0500

    Add new action alert_on_door_left_open

---
+++ automation/jsr223/aaa_modules/layout_model/actions/alert_on_door_left_open.py
@@ -0,0 +1,71 @@
+from threading import Timer
+
+from aaa_modules.alert import Alert
+from aaa_modules.alert_manager import AlertManager
+from aaa_modules.layout_model.actions.action import Action
+from aaa_modules.layout_model.devices.contact import Door
+from aaa_modules.platform_encapsulator import PlatformEncapsulator as PE
+
+class AlertOnExternalDoorLeftOpen(Action):
+
+    '''
+    Send an warning alert if a door on an external zone has been left open for
+    a period of time.
+    Triggered when a door is open (--> sart the timer), or when a door is
+    closed (--> stop the timer)
+    '''
+
+    def __init__(self, maxElapsedTimeInSeconds = 30 * 60):
+        '''
+        Ctor
+
+        :param int maxElapsedTimeInSeconds: the elapsed time in second since
+            a door has been open, and at which point an alert will be sent
+        :raise ValueError: if any parameter is invalid
+        '''
+
+        if maxElapsedTimeInSeconds <= 0:
+            raise ValueError('maxElapsedTimeInSeconds must be positive')
+
+        self.timers = {}
+        self.maxElapsedTimeInSeconds = maxElapsedTimeInSeconds
+
+    def onAction(self, events, zone, getZoneByIdFn):
+        if not zone.isExternal():
+            return False
+
+        doors = zone.getDevicesByType(Door)
+        if len(doors) == 0:
+            return False
+
+        def sendAlert():
+            msg = 'The {} door has been opened for {} minutes.'.format(
+                    zone.getName(), self.maxElapsedTimeInSeconds / 60)
+            PE.logInfo(msg)
+
+            alert = Alert.createWarningAlert(msg)
+            AlertManager.processAlert(alert)
+
+        for door in doors:
+            timer = self.timers[door] if door in self.timers else None
+
+            if door.isOpen():
+                if None != timer:
+                    timer.cancel()
+                    del self.timers[door]
+
+                timer = Timer(self.maxElapsedTimeInSeconds, sendAlert)
+                timer.start()
+                self.timers[door] = timer
+            else:
+                if None != timer:
+                    timer.cancel()
+                    del self.timers[door]
+
+        return True
+
+    def hasRunningTimer(self):
+        '''
+        Returns true if at least one timer is running.
+        '''
+        return any(t.isAlive() for t in self.timers.values())
---
+++ automation/jsr223/aaa_modules/layout_model/actions/alert_on_door_left_open_test.py
@@ -0,0 +1,78 @@
+import time
+
+from core.jsr223 import scope
+from org.eclipse.smarthome.core.library.items import SwitchItem
+
+from aaa_modules.alert_manager import AlertManager
+from aaa_modules.platform_encapsulator import PlatformEncapsulator as PE
+
+from aaa_modules.layout_model.zone import Zone, Level
+from aaa_modules.layout_model.devices.contact import Door
+from aaa_modules.layout_model.device_test import DeviceTest
+
+#from aaa_modules.layout_model.actions import alert_on_door_left_open
+#reload(alert_on_door_left_open)
+from aaa_modules.layout_model.actions.alert_on_door_left_open import AlertOnExternalDoorLeftOpen
+
+ITEMS = [SwitchItem('Door1'), SwitchItem('Door2')]
+
+# Unit tests for zone_manager.py.
+class AlertOnExternalDoorLeftOpenTest(DeviceTest):
+    def setUp(self):
+        self.zone1 = Zone.createExternalZone('porch').addDevice(Door(ITEMS[0]))
+        self.zone2 = Zone.createExternalZone('garage').addDevice(Door(ITEMS[1]))
+
+        AlertManager._setTestMode(True)
+        AlertManager.reset()
+
+    def tearDown(self):
+        AlertManager._setTestMode(False)
+
+    def getItems(self, resetState = False):
+        if resetState:
+            for item in ITEMS:
+                if isinstance(item, SwitchItem):
+                    item.setState(scope.OnOffType.OFF)
+                elif isinstance(item, SwitchItem):
+                    item.setState(UndefState)
+
+        return ITEMS
+
+    def testOnAction_notAnExternalZone_returnsFalse(self):
+        value = AlertOnExternalDoorLeftOpen().onAction(
+                events, Zone('innerZone'), None)
+        self.assertFalse(value)
+
+    def testOnAction_externalZoneWithNoDoor_returnsFalseAndTimerStarted(self):
+        value = AlertOnExternalDoorLeftOpen().onAction(
+                events, Zone.createExternalZone('aZone'), None)
+        self.assertFalse(value)
+
+    def testOnAction_aDoorIsOpen_returnsTrue(self):
+        ITEMS[0].setState(scope.OnOffType.ON)
+
+        action = AlertOnExternalDoorLeftOpen(1)
+        value = action.onAction(events, self.zone1, None)
+
+        self.assertTrue(value)
+        self.assertTrue(action.hasRunningTimer())
+
+        time.sleep(2)
+        self.assertTrue("door" in AlertManager._lastEmailedSubject)
+
+    def testOnAction_aDoorWasOpenButClosedSoonAfter_returnsTrueAndTimerCancelled(self):
+        ITEMS[0].setState(scope.OnOffType.ON)
+
+        action = AlertOnExternalDoorLeftOpen()
+        value = action.onAction(events, self.zone1, None)
+
+        self.assertTrue(value)
+        self.assertTrue(action.hasRunningTimer())
+
+        # simulate door closed
+        ITEMS[0].setState(scope.OnOffType.OFF)
+        value = action.onAction(events, self.zone1, None)
+        self.assertTrue(value)
+        self.assertFalse(action.hasRunningTimer())
+
+PE.runUnitTest(AlertOnExternalDoorLeftOpenTest)
